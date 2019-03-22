688a3b0939593daa7aa78157ee3691de78aad45f
Evgeniy Muravitskiy <evgeniy.muravitskiy@tensor.technology>
2019-03-22 12:43:10 +0300

    Add plugin submodule

--- idea-plugin/src/main/kotlin/HelloWorld.kt
+++ idea-plugin/src/main/kotlin/HelloWorld.kt
@@ -2,12 +2,13 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.Messages
 
-import io.github.props.converter.core.SuperHellower;
+import io.github.props.converter.core.SuperHellower
 
 class HelloWorld: AnAction("Hasdaksljd") {
     override fun actionPerformed(event: AnActionEvent?) {
         val project = event?.project
 //        println(sayHi())
+        println {project?.name?: "lol"}
         println(SuperHellower().sayHello())
         Messages.showMessageDialog(project, "Hello world!", "Greeting", Messages.getInformationIcon())
     }
